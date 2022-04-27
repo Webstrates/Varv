1b764eea119b4ec603115d610c389ceb23e554b5
CAVI-Jenkins <support@cavi.au.dk>
2022-04-27 13:37:13 +0000

    Automatic commit

--- integration/cauldron-delayloader/cauldron-delayloader.js
+++ integration/cauldron-delayloader/cauldron-delayloader.js
@@ -1,7 +1,13 @@
 // Run when/if Cauldron is initialized
-EventSystem.registerEventCallback("Cauldron.OnOpen", async ()=>{
-        await wpm.require("varv-cauldron");
-        
-        // Cause a restart to update the TreeBrowser with the current Concepts
-        EventSystem.triggerEvent("Varv.Restart");
-});
+{
+    let alreadyInitializedDelayLoader = false;
+    EventSystem.registerEventCallback("Cauldron.OnOpen", async ()=>{
+            if (!alreadyInitializedDelayLoader){
+                await wpm.require("varv-cauldron");
+
+                // Cause a restart to update the TreeBrowser with the current Concepts
+                EventSystem.triggerEvent("Varv.Restart");
+                alreadyInitializedDelayLoader = true;
+            }
+    });    
+}
